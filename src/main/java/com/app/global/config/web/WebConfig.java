ea47bfbdfa1bb6e238d2417bc8fbd7ece27fa0c5
valeos <code.juno@gmail.com>
2022-12-02 16:04:17 +0900

    applicatoin.yml added

--- src/main/java/com/app/global/config/web/WebConfig.java
+++ src/main/java/com/app/global/config/web/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
        registry.addMapping("/api/**")
-               .allowedOrigins("http://localhost:8082")
+               .allowedOrigins("*")
                .allowedMethods(
                        HttpMethod.GET.name(),
                        HttpMethod.POST.name(),
