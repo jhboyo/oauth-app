d25b015e4d570b9c19d20bf57da6be96b36d6c44
valeos <code.juno@gmail.com>
2022-12-11 22:23:35 +0900

    logout added

---
+++ src/main/java/com/app/api/logout/controller/LogoutController.java
@@ -0,0 +1,35 @@
+package com.app.api.logout.controller;
+
+
+import com.app.api.logout.service.LogoutService;
+import com.app.global.util.AuthorizationHeaderUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api")
+public class LogoutController {
+
+    private final LogoutService logoutService;
+
+    public LogoutController(LogoutService logoutService) {
+        this.logoutService = logoutService;
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletRequest httpServletRequest) {
+        String authorizationHeader = httpServletRequest.getHeader("Authorization");
+
+        AuthorizationHeaderUtils.validateAuthorization(authorizationHeader);
+
+        String accessToken = authorizationHeader.split(" ")[1];
+
+        logoutService.logout(accessToken);
+
+        return ResponseEntity.ok("logout success");
+    }
+}
--- src/main/java/com/app/global/error/ErrorCode.java
+++ src/main/java/com/app/global/error/ErrorCode.java
@@ -15,10 +15,13 @@ public enum ErrorCode {
     NOT_VALID_BEARER_GRANT_TYPE(HttpStatus.UNAUTHORIZED, "A-004" ,"인증타입에 Bearer타입이 아닙니다." ),
     REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "A-005", "해당 Refresh Token은 존재하지 않습니다."),
     REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "A-006", "해당 Refresh Tokend은 만료 되었습니다."),
+    NOT_ACCESS_TOKEN_TYPE(HttpStatus.UNAUTHORIZED, "A-007", "해당 토큰은 Access Token 이 아닙니다."),
 
     // 회원,
-    INVALID_MEMBER_TYPE(HttpStatus.BAD_REQUEST, "M-101" , "잘못 된 회원 타입입니다.(MemberType - Kakao" ),
-    ALREADY_REGISTER_MEMBER(HttpStatus.BAD_REQUEST, "M-002", "이미 가입 된 회원입니다.");
+    INVALID_MEMBER_TYPE(HttpStatus.BAD_REQUEST, "M-101", "잘못 된 회원 타입입니다.(MemberType - Kakao" ),
+    ALREADY_REGISTER_MEMBER(HttpStatus.BAD_REQUEST, "M-002", "이미 가입 된 회원입니다."),
+    MEMBER_NOT_EXIST(HttpStatus.BAD_REQUEST,"M-003", "해당 회원은 존재하지 않습니다." );
+
     ;
 
     private final HttpStatus httpStatus;
