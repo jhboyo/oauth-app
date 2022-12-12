effa8817e7a13c587c0310e95e971769d288a990
valeos <code.juno@gmail.com>
2022-12-13 01:46:04 +0900

    BaseTest refactor

--- src/test/java/com/app/common/BaseTest.java
+++ src/test/java/com/app/common/BaseTest.java
@@ -1,10 +1,9 @@
 package com.app.common;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.modelmapper.ModelMapper;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -12,23 +11,20 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 
-@Disabled
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@AutoConfigureRestDocs
 @ActiveProfiles("test")
 public class BaseTest {
 
-    protected final MockMvc mockMvc;
+    @Autowired
+    protected  MockMvc mockMvc;
 
-    protected final ObjectMapper objectMapper;
+    @Autowired
+    protected ObjectMapper objectMapper;
 
-    protected final ModelMapper modelMapper;
+    @Autowired
+    protected  ModelMapper modelMapper;
 
-    public BaseTest(MockMvc mockMvc, ObjectMapper objectMapper, ModelMapper modelMapper) {
-        this.mockMvc = mockMvc;
-        this.objectMapper = objectMapper;
-        this.modelMapper = modelMapper;
-    }
+
 }
