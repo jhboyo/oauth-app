661be65e1f7cc8ce79ccaf0742d1b66a398307b1
valeos <code.juno@gmail.com>
2022-12-18 19:12:35 +0900

    회원조회 mockmvc 테스트 완료

--- src/main/java/com/app/api/member/controller/MemberInfoController.java
+++ src/main/java/com/app/api/member/controller/MemberInfoController.java
@@ -1,16 +1,12 @@
 package com.app.api.member.controller;
 
-import antlr.Token;
 import com.app.api.member.dto.MemberInfoResponseDto;
 import com.app.api.member.service.MemberInfoService;
-import com.app.global.jwt.service.TokenManager;
 import com.app.global.resolver.memberinfo.MemberInfo;
 import com.app.global.resolver.memberinfo.MemberInfoDto;
-import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,12 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberInfoController {
 
     private final MemberInfoService memberInfoService;
-    private final TokenManager tokenManager;
-
-    public MemberInfoController(MemberInfoService memberInfoService, TokenManager tokenManager) {
+    public MemberInfoController(MemberInfoService memberInfoService) {
         this.memberInfoService = memberInfoService;
-        this.tokenManager = tokenManager;
     }
+
+
+
 
     /* 아래 ResolveArguementHandler를 사용한 방식으로 변경 -> 어노테이션 생서으로 반복코드 줄임
     @GetMapping("/info")
