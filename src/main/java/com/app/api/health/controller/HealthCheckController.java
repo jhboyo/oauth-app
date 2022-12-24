c25cd051ec531dd5fd42f3ec5374b8a20fd4c665
valeos <code.juno@gmail.com>
2022-12-25 08:17:25 +0900

    polishing...

--- src/main/java/com/app/api/health/controller/HealthCheckController.java
+++ src/main/java/com/app/api/health/controller/HealthCheckController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class HealthCheckController {
 
     @Tag(name="health check")
     @Operation(summary = "서버 Health Check API", description = "현재 서버가 정상적으로 기동이 된 상태인지 검사하는 API")
-    @RequestMapping("/health")
+    @GetMapping("/health")
     public ResponseEntity<HealthCheckResponseDto> healthCheck() {
         HealthCheckResponseDto healthCheckResponseDto = HealthCheckResponseDto.builder()
                 .health("ok")
